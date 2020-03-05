aa5157f456350e671277e64428fc5737d1fe3689
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2020-03-04 22:04:27 -0300

    PostForm.js com evente criado e pronto para o disparo

--- src/PostForm.js
+++ src/PostForm.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
 
 class PostForm extends Component {
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const title = this.getTitle.value;
+        const message = this.getMessage.value;
+        const data = {
+            id: new Date(),
+            title,
+            message
+        }
+        console.log(data)
+    }
     render() {
         return (
             <div>
                 <h1>Poste a sua sugestão para a Smarkio:</h1>
-                <form>
-                    <input required type="text" placeholder="Digite um título para sua sugestão" /><br /><br />
-                    <textarea required rows="5" cols="28" placeholder="Escreva sua sugestão" /><br /><br />
+                <form onSubmit={this.handleSubmit}>
+                    <input 
+                        required 
+                        type="text"
+                        ref={ (input) => this.getTitle = input} 
+                        placeholder="Digite um título para sua sugestão" 
+                    />
+                    <br /><br />
+                    <textarea 
+                        required 
+                        rows="5" 
+                        cols="28"
+                        ref={ (input) => this.getMessage = input} 
+                        placeholder="Escreva sua sugestão" 
+                    />
+                    <br /><br />
                     <button>Publicar</button>
                 </form>
             </div>
