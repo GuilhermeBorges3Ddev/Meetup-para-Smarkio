9ba4b642f611200f6988969ff7e5bc24c185200d
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2020-03-04 21:20:28 -0300

    index.js adaptada e reducer basilar instanciado

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { createStore } from 'redux';
+
+//Aqui nós criamos a variável que recebe nosso container de estado inicialmente vazio
+const store = createStore();
+
 ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/reducers/postReducer.js
@@ -0,0 +1,4 @@
+//Nessa arrow temos a constante que espera um array de actions para disparar os estados
+const postReducer = (state = [], action) => {}
+
+export default postReducer;
